0bac15ba7ee32de408ab2256e5ab6021a508c68a
Weissbier <weissbier@ernstchan.net>
2010-07-01 02:35:08 +0200

    do it nicer :3

--- dev/futaba_style.pl
+++ dev/futaba_style.pl
@@ -10,7 +10,7 @@ use constant NORMAL_HEAD_INCLUDE => q{
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-<title><if $title><var $title> - </if><var $subject> <const TITLE></title>
+<title><const TITLE> &raquo; <if $title><var $title></if><if !$title>/dev/ - Phutaba 1</if></title>
 <meta http-equiv="Content-Type" content="text/html;charset=<const CHARSET>" />
 <link rel="shortcut icon" href="favicon.ico" />
 
