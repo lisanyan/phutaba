3cfc17a148c87752585c010ca870be9c4d3095d2
weissbier <weissbier@ernstchan.com>
2015-11-22 02:12:06 +0100

    GOURANGA

--- sql/news.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `news` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `date` datetime NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
